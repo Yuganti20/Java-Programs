0e999439ade22316df411c0b39ec2f0a3a161b75
Yuganti20 <119279467+Yuganti20@users.noreply.github.com>
2024-01-10 15:08:49 +0530

    How to swap 2 no without using 3rd variable Program in java

--- Yuganti.java
+++ Yuganti.java
@@ -7,16 +7,16 @@ public class Yuganti {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int x, y, temp;
-		Scanner sc = new Scanner(System.in);
+		int x, y;
+		Scanner sc = new Scanner(System.in);  
 		System.out.println("Enter the no x and y before swapping:");
 		x = sc.nextInt();
 		y = sc.nextInt();
 		System.out.println("Before Swapping\nx = " + x + "\ny = " + y);
 
-		temp = x;
-		x = y;
-		temp = y;
+		x=x+y;
+		y=x-y;
+		x=x-y;
 
 		System.out.println("After swapping\nx = " + x + "\ny = " + y);
 
